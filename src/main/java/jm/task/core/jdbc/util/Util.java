52118a068a96187e254bf3a7610617f6a176a665
Nikolay <jejel.n.o@mail.ru>
2023-08-15 22:30:32 +0300

    solution of problem 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/forkata";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("We are connected!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("there is no connection... Exception!");
+        }
+        return connection;
+    }
 }
