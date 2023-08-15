e84f62b72f0bc51ceaec94f9818a843e0757beee
Nikolay <jejel.n.o@mail.ru>
2023-08-16 02:43:45 +0300

    test 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,12 @@
 package jm.task.core.jdbc.util;
 
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,20 +15,45 @@ public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/forkata";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
+    private static SessionFactory sessionFactory = null;
 
+    public static SessionFactory getConnection() {
 
-
-    public static Connection getConnection() {
-        Connection connection = null;
         try {
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("We are connected!");
-            }
-        } catch (SQLException e) {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.url", DB_URL)
+                    .setProperty("hibernate.connection.username", DB_USERNAME)
+                    .setProperty("hibernate.connection.password", DB_PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable e) {
             e.printStackTrace();
-            System.out.println("there is no connection... Exception!");
         }
-        return connection;
+        return sessionFactory;
+    }
+
+    public static void closeConnection() {
+        if (sessionFactory != null)
+            sessionFactory.close();
     }
 }
+
+
+//    public static Connection getConnection() {
+//        Connection connection = null;
+//        try {
+//            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+//            if (!connection.isClosed()) {
+//                System.out.println("We are connected!");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            System.out.println("there is no connection... Exception!");
+//        }
+//        return connection;
+//    }
+//}
